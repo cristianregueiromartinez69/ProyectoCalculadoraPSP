ad68abe6938d198e689cdaefb568f68fd4643a23
cristian <cregueiromartinez@danielcastelao.org>
2025-01-25 10:30:11 +0100

    preparando suma para servidor

--- src/Cliente.java
+++ src/Cliente.java
@@ -6,5 +6,21 @@ public class Cliente {
 
     public static void main(String[] args) {
 
+        int puerto = 6666;
+        String [] datosOperar = new String [2];
+
+        try(Socket socket = new Socket("localhost", puerto)){
+
+            System.out.println("Conexion establecida con el servidor");
+
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+
+
+
+        }catch(IOException e){
+            System.out.println("Ups, error al conectarse al socket del servidor");
+        }
+
     }
 }
