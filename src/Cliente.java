f6a19a5b07048a57f51cb8eff54421e571a4c104
cristian <cregueiromartinez@danielcastelao.org>
2025-01-25 10:25:20 +0100

    haciendo las clases cliente y servidor

---
+++ src/Cliente.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Cliente {
+
+    public static void main(String[] args) {
+
+    }
+}
