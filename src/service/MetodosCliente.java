4b8baf92a70b4fe2d3144ecdbe5b4f58acf59f3a
cristian <cregueiromartinez@danielcastelao.org>
2025-01-25 10:56:25 +0100

    comprobacion correcta digitos de cliente

--- src/service/MetodosCliente.java
+++ src/service/MetodosCliente.java
@@ -1,4 +1,25 @@
 package service;
 
+import java.util.Scanner;
+
 public class MetodosCliente {
+
+    public void operacionParaServidor(String[] operacion){
+
+        System.out.println("Introdudce el primer número de la operación: ");
+        Scanner sc = new Scanner(System.in);
+        String numero1 = sc.nextLine();
+
+    }
+
+    public boolean comprobarDigitos(String numero){
+        String [] numeros = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+        for (String s : numeros) {
+            if (s.equals(numero)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
