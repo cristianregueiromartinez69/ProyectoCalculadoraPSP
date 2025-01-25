f12f6c744fb8c15fb08127057df06239e6a3af88
cristian <cregueiromartinez@danielcastelao.org>
2025-01-25 11:01:44 +0100

    comprobacion correcta operacion de cliente

--- src/service/MetodosCliente.java
+++ src/service/MetodosCliente.java
@@ -10,12 +10,30 @@ public class MetodosCliente {
         Scanner sc = new Scanner(System.in);
         String numero1 = sc.nextLine();
 
+        while(comprobarDigitos(numero1)){
+            System.out.println("Número incorrecto, por favor, introduce un número válido: ");
+            numero1 = sc.nextLine();
+        }
+
+        System.out.println("Introduce una operación: ");
+        String auxOperacion = sc.nextLine();
+
     }
 
     public boolean comprobarDigitos(String numero){
         String [] numeros = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
         for (String s : numeros) {
             if (s.equals(numero)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean comprobarOperacion(String operacion){
+        String [] operaciones = {"+", "-", "/", "*"};
+        for (String s : operaciones) {
+            if (s.equals(operacion)) {
                 return true;
             }
         }
